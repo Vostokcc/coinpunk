e858f3318413c7cd51147ab23ec3ad18af73c513
Kyle Drake <kyledrake@gmail.com>
2013-06-06 00:36:46 -0700

    beginning work on js crypto. Implemented pbkdf2 for hashing password for nacl securebox key

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,29 +1,37 @@
-//= require underscore
-//= require jquery
-//= require bootstrap
-//= require jstz
+//= require lib/underscore
+//= require lib/jquery
+//= require lib/bootstrap
+//= require lib/jstz
 
-//= require bitcoinjs-lib/crypto-js/crypto.js
-//= require bitcoinjs-lib/crypto-js/sha256.js
-//= require bitcoinjs-lib/crypto-js/ripemd160.js
+//= require lib/crypto-js/crypto.js
+//= require lib/crypto-js/sha256.js
+//= require lib/crypto-js/ripemd160.js
+//= require lib/crypto-js/pbkdf2.js
 
-//= require bitcoinjs-lib/jsbn/prng4.js
-//= require bitcoinjs-lib/jsbn/rng.js
-//= require bitcoinjs-lib/jsbn/jsbn.js
-//= require bitcoinjs-lib/jsbn/jsbn2.js
-//= require bitcoinjs-lib/jsbn/ec.js
-//= require bitcoinjs-lib/jsbn/sec.js
+//= require lib/jsbn/prng4.js
+//= require lib/jsbn/rng.js
+//= require lib/jsbn/jsbn.js
+//= require lib/jsbn/jsbn2.js
+//= require lib/jsbn/ec.js
+//= require lib/jsbn/sec.js
 
-//= require bitcoinjs-lib/events/eventemitter.js
+//= require lib/events/eventemitter.js
 
-//= require bitcoinjs-lib/bitcoin.js
-//= require bitcoinjs-lib/util.js
-//= require bitcoinjs-lib/base58.js
-//= require bitcoinjs-lib/address.js
-//= require bitcoinjs-lib/ecdsa.js
-//= require bitcoinjs-lib/eckey.js
-//= require bitcoinjs-lib/opcode.js
-//= require bitcoinjs-lib/script.js
-//= require bitcoinjs-lib/transaction.js
-//= require bitcoinjs-lib/wallet.js
-//= require bitcoinjs-lib/txdb.js
+//= require lib/bitcoinjs-lib/bitcoin.js
+//= require lib/bitcoinjs-lib/util.js
+//= require lib/bitcoinjs-lib/base58.js
+//= require lib/bitcoinjs-lib/address.js
+//= require lib/bitcoinjs-lib/ecdsa.js
+//= require lib/bitcoinjs-lib/eckey.js
+//= require lib/bitcoinjs-lib/opcode.js
+//= require lib/bitcoinjs-lib/script.js
+//= require lib/bitcoinjs-lib/transaction.js
+//= require lib/bitcoinjs-lib/wallet.js
+//= require lib/bitcoinjs-lib/txdb.js
+
+//= require lib/js-nacl/nacl.js
+//= require lib/sjcl.js
+//= require lib/sjcl-sha512.js
+
+//= require coinpunk/coinpunk.js
+//= require coinpunk/pbkdf2.js
