34d274e5af2ab1470a43290b9472d32f831c7432
Kyle Drake <kyledrake@gmail.com>
2013-06-05 17:46:01 -0700

    add bitcoinjs-lib

--- assets/js/all.js
+++ assets/js/all.js
@@ -2,3 +2,28 @@
 //= require jquery
 //= require bootstrap
 //= require jstz
+
+//= require bitcoinjs-lib/crypto-js/crypto.js
+//= require bitcoinjs-lib/crypto-js/sha256.js
+//= require bitcoinjs-lib/crypto-js/ripemd160.js
+
+//= require bitcoinjs-lib/jsbn/prng4.js
+//= require bitcoinjs-lib/jsbn/rng.js
+//= require bitcoinjs-lib/jsbn/jsbn.js
+//= require bitcoinjs-lib/jsbn/jsbn2.js
+//= require bitcoinjs-lib/jsbn/ec.js
+//= require bitcoinjs-lib/jsbn/sec.js
+
+//= require bitcoinjs-lib/events/eventemitter.js
+
+//= require bitcoinjs-lib/bitcoin.js
+//= require bitcoinjs-lib/util.js
+//= require bitcoinjs-lib/base58.js
+//= require bitcoinjs-lib/address.js
+//= require bitcoinjs-lib/ecdsa.js
+//= require bitcoinjs-lib/eckey.js
+//= require bitcoinjs-lib/opcode.js
+//= require bitcoinjs-lib/script.js
+//= require bitcoinjs-lib/transaction.js
+//= require bitcoinjs-lib/wallet.js
+//= require bitcoinjs-lib/txdb.js
